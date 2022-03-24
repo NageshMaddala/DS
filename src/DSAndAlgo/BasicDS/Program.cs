47f65e8b0cc461b709c1fa3d682d681bfff72550
Nagesh_Maddala <nagesh_maddala@dell.com>
2022-03-23 20:36:51 -0500

    Basic code

---
+++ src/DSAndAlgo/BasicDS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicDS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
