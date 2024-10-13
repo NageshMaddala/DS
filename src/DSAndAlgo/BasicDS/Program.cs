a28cc95358f2695eebd98b5a67d6b99afee62d0e
NageshMaddala <nageshvmr@gmail.com>
2024-10-13 19:37:50 -0400

    Sample TrieII

--- src/DSAndAlgo/BasicDS/Program.cs
+++ src/DSAndAlgo/BasicDS/Program.cs
@@ -1,4 +1,5 @@
-﻿using DSLib.DataStructures;
+﻿using DS_Lib;
+using DSLib.DataStructures;
 using System;
 
 namespace BasicDS
@@ -8,6 +9,7 @@ namespace BasicDS
         static void Main(string[] args)
         {
             //SamplesRelatedToTrie();
+            SamplesRelatedToTrieII();
         }
 
         public static void SamplesRelatedToTrie()
@@ -24,6 +26,26 @@ namespace BasicDS
             Console.WriteLine($"Search if Strawberry exists in trie: {(trie.Search("strawberry") ? true : false)}");
             Console.WriteLine($"Search if Strike exists in trie: {(trie.Search("strike") ? true : false)}");
             Console.WriteLine($"Search if Stri starts with in trie: {(trie.StartsWith("stri") ? true : false)}");
+        }
+
+        public static void SamplesRelatedToTrieII()
+        {
+            TrieII trieII = new TrieII();
+
+            trieII.InsertWord("apple");
+            trieII.InsertWord("apple");
+            trieII.InsertWord("apps");
+            trieII.InsertWord("apxl");
+            trieII.InsertWord("apxl");
+
+            var count = trieII.GetExactMatchingWordCount("apps"); //1
+            var count0 = trieII.GetExactMatchingWordCount("apxl"); //2
+            var count1 = trieII.GetStartsWithWordCount("apx"); //2
+            var count3 = trieII.GetStartsWithWordCount("ap"); //5
+            var count4 = trieII.GetExactMatchingWordCount("apple"); // 2
+
+            trieII.EraseWord("apple");
+            var count5 = trieII.GetExactMatchingWordCount("apple");
         }
     }
 }
---
+++ src/DSAndAlgo/DS-Lib/TrieII.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS_Lib
+{
+    public class NodeII
+    {
+        NodeII[] _links = new NodeII[26];
+        bool flag = false;
+        int _counterEndWith = 0;
+        int _counterPrefix = 0;
+
+        public void Put(char ch, NodeII node)
+        {
+            _links[ch - 'a'] = node;
+            //node._counterPrefix++;
+        }
+
+        public int GetPrefix()
+        {
+            return _counterPrefix;
+        }
+
+        public void IncreasePrefix()
+        {
+            _counterPrefix++;
+        }
+
+        public void DecreasePrefix()
+        {
+            _counterPrefix--;
+        }
+
+        public int GetEndWith()
+        {
+            return _counterEndWith;
+        }
+
+        public void IncreaseEnd()
+        {
+            _counterEndWith++;
+        }
+
+        public void DecreaseEnd()
+        {
+            _counterEndWith--;
+        }
+
+        public bool ContainsKey(char ch)
+        {
+            return _links[ch - 'a'] != null;
+        }
+
+        public NodeII Get(char ch)
+        {
+            return _links[ch - 'a'];
+        }
+    }
+
+    public interface ITrieII
+    {
+        public void InsertWord(string word);
+
+        public int GetExactMatchingWordCount(string word);
+
+        public int GetStartsWithWordCount(string word);
+
+        public void EraseWord(string word);
+    }
+
+    public class TrieII : ITrieII
+    {
+        private NodeII _root;
+
+        public TrieII()
+        {
+            _root = new NodeII();
+        }
+
+        public void EraseWord(string word)
+        {
+            NodeII node = _root;
+
+            for(int i =0; i< word.Length; i++)
+            {
+                if (!node.ContainsKey(word[i]))
+                    return;
+
+                node = node.Get(word[i]);
+                node.DecreasePrefix();
+            }
+            node.DecreaseEnd();
+        }
+
+        public int GetExactMatchingWordCount(string word)
+        {
+            NodeII node = _root;
+
+            for(int i=0; i< word.Length; i++)
+            {
+                if (!node.ContainsKey(word[i]))
+                    return 0;
+
+                node = node.Get(word[i]);
+            }
+
+            return node.GetEndWith();
+        }
+
+        public int GetStartsWithWordCount(string word)
+        {
+            NodeII node = _root;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (!node.ContainsKey(word[i]))
+                    return 0;
+
+                node = node.Get(word[i]);
+            }
+            return node.GetPrefix();
+        }
+
+        public void InsertWord(string word)
+        {
+            NodeII node = _root;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (!node.ContainsKey(word[i]))
+                    node.Put(word[i], new NodeII());               
+
+                node = node.Get(word[i]);
+                node.IncreasePrefix();
+            }
+
+            node.IncreaseEnd();
+        }
+
+        
+    }
+}
